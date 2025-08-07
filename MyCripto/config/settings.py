727cbcc22208877101a50a7ac986c91f65acc77d
Zypher <nesicking@gmail.com>
2025-08-07 17:05:44 +0200

    Update settings.py

--- MyCripto/config/settings.py
+++ MyCripto/config/settings.py
@@ -4,7 +4,7 @@ COIN_NAME = "MineByte"
 COIN_SYMBOL = "MB"
 INITIAL_BALANCE = 100
 MINING_REWARD = 10
-DIFFICULTY = 4  
+DIFFICULTY = 6  
 
 WALLET_FILE = "data/wallets.json"
 BLOCKCHAIN_FILE = "data/blockchain.json"
